95e8506e6c98c2bf0449c7d1758d40315dfa6e54
Gaurav Gosain <itsgauravgosain@gmail.com>
2025-10-23 22:34:29 +0400

    chore: update nix package to v0.0.22 with updated vendor hash

--- tuios.nix
+++ tuios.nix
@@ -2,11 +2,11 @@
 
 pkgs.buildGoModule {
   pname = "tuios";
-  version = "v0.0.19";
+  version = "v0.0.22";
 
   src = ./.;
 
   # This has to be updated each time dependencies are updated.
   # Use pkgs.lib.fakeHash
-  vendorHash = "sha256-yF7sYqAvrajrQllJXU0VZwxuXtSJZPuCl/cvZTD2WJA=";
+  vendorHash = "sha256-h3MRdo03okkPBDdB8/RCVaKhOGdGQP3tPkXI6V6Hn2g=";
 }
