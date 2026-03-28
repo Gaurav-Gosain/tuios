9c9771a9845b4d38d6a7bef27a97f5fa4693714f
Gaurav Gosain <itsgauravgosain@gmail.com>
2026-03-29 01:30:06 +0400

    fix: nix build - set GOTOOLCHAIN=auto, update version to v0.7.0
    The nixpkgs Go (1.25.1) is older than what go.mod requires (1.25.8).
    Setting GOTOOLCHAIN=auto lets Go download the needed toolchain.
    Also set vendorHash to fakeHash - needs to be updated from a failed
    build log on next tag.

--- tuios.nix
+++ tuios.nix
@@ -2,11 +2,15 @@
 
 pkgs.buildGoModule {
   pname = "tuios";
-  version = "v0.6.0";
+  version = "v0.7.0";
 
   src = ./.;
 
+  # Allow Go to download the required toolchain version if the
+  # nixpkgs Go is older than what go.mod specifies.
+  env.GOTOOLCHAIN = "auto";
+
   # This has to be updated each time dependencies are updated.
-  # Use pkgs.lib.fakeHash
-  vendorHash = "sha256-kDZRT/Ua+SaxyZ6RI9ZY2tqBgQBWo755fvQVRupBsUc=";
+  # Use pkgs.lib.fakeHash to get the correct hash from a failed build.
+  vendorHash = pkgs.lib.fakeHash;
 }
